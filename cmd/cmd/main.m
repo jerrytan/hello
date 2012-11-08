1a6223569ec1084d89b6293bd82745aaa23c9cfe
tan zhongyi <tanzhongyi@tanmatoMacBook-Air.local>
2012-11-08 21:24:01 +0800

    my first commit from Xcode, using cmdtool template

---
+++ cmd/cmd/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  cmd
+//
+//  Created by tan zhongyi on 12-11-8.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
